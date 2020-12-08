db30c14e5eee9523520e9fd46e8251a7aae6d5b4
Vi PC <62630060+vi-pc@users.noreply.github.com>
2020-12-08 18:32:21 +0700

    Create notation.md ML-RL first notation

---
+++ AI/machine-learning/reinforcement-learning/notation.m
@@ -0,0 +1,27 @@
+Notation for Reinforcement Learning
+
+- Capital letters are used for random variables and major algorithm variables.
+- Lower case letters are used for the values of random variables and for scalar functions.
+- Bold and lowoers letters are required to be real-valued vectors.
+
+
+| Notation | Meaning|
+| :-- | :---------------------------------|
+| s | state |
+| a | action |
+| S | set of all nonterminal states|
+| S+ | set of all states, including the terminal state|
+| A(s) | set of actions possible in states|
+| R | set of possible rewardstdiscrete time step|
+| | |
+| T | final time step of an episode|
+| S_t| state at t |
+| A_t | action at t |
+| R_t | reward at t |
+| G_t | return (cumulative discounted reward) following t|
+|...|...|
+| | |
+|π|policy, decision-making rule |
+|π(s)| action taken in states under deterministic policy π|
+| γ | discount-rate parameter |
+(.updating)
